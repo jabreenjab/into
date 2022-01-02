09ffb4d5e89016b3563b90b9ce92aea0e4a7def9
jabreenjab <jabreenahmed>
2022-01-02 11:06:16 +0500

    just all few lines.

--- introduction.r
+++ introduction.r
@@ -1,7 +1,15 @@
 # into
 #This includes some of my brief introduction.
-name= "My name is jabreen ahmad."
-profession= "My profession is teaching.
-print(name)
-print(profession)
 
+name="My name is jabreen ahmad"
+      
+prof="My profession is teaching"
+
+print (name)
+
+print (prof)
+
+print("what are you doing here")
+
+jabreen= ("hy dude")
+print(jabreen)
